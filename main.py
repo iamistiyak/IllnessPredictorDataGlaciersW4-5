c4bdf1e89fb6d00eac519f6542afe5c5e18cb230
Istiyak Shaikh <86108816+iamistiyak@users.noreply.github.com>
2025-04-06 19:06:31 +0100

    Final Commit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def home():
     return render_template("index.html", cities=cities, genders=genders)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='0.0.0.0', debug=True)
